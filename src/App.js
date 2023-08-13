2863d0d9c07394f5db31abc01fb96cb2f1bc9c6e
Javeria731 <jia_aamir@hotmail.com>
2023-08-13 12:32:28 -0400

    Added user feedback

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Switch, Routes, Route, BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Updated imports
 import Header from './Components/pageHeader/Header';
 import VideoContainer from './Components/VideoContainer/VideoContainer';
 import './styles/main.scss';
 import Upload from './pages/Upload';
-import success from'./pages/success';
+import Success from './pages/success'; // Corrected import
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-      
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<VideoContainer />} />
         <Route path="/upload" element={<Upload />} />
-        <Route exact path="/videos/:videoId" element={<VideoContainer />} />
-        <Route path="/success" element ={<success />}/>
+        <Route path="/videos/:videoId" element={<VideoContainer />} />
+        <Route path="/success" element={<Success />} /> 
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
-      /* <Routes>
-        <Route exact path="/" element={<VideoContainer />} />
-        <Route exact path="/Upload" element={<Upload />} />
-        <Route exact path="/video/:videoId" element={<VideoContainer />} />
-      </Routes> */
   );
 }
 
